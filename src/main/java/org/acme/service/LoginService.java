86418afab06827d6d3f670504682d296150880b5
Syahril Ramadhan <rodyulo20@gmail.com>
2023-07-21 11:29:25 +0700

    Update LoginService.java

--- src/main/java/org/acme/service/LoginService.java
+++ src/main/java/org/acme/service/LoginService.java
@@ -10,7 +10,9 @@ import org.acme.repository.LoginRepository;
 import org.acme.repository.UserRepository;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class LoginService {
